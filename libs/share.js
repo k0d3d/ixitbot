93a772b9edfa3f926a5e4ad969ec8cee22870c4b
Mykel Koded <michael.rhema@gmail.com>
2016-02-04 18:21:40 +0100

    it tweets! twitter sdk tweets,

---
+++ libs/share.js
@@ -0,0 +1,22 @@
+var Twitter = require('twitter');
+
+function SocialClass (title, body, attach) {
+
+}
+
+SocialClass.prototype.constructor = SocialClass;
+
+SocialClass.prototype.tweet = function tweet (params) {
+  var client = new Twitter({
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  });
+
+  client.post('statuses/update', params, function (error, tweet, response) {
+    console.log(response.body);
+  });
+};
+
+module.exports = SocialClass;
